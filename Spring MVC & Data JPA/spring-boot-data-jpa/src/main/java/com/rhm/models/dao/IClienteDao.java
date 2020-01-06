6b579e8a8d844326d0f4ab01af4d97d3b1fb16d6
rmendez <rmendez@modernizacion.gob.ar>
2020-01-06 07:57:52 -0300

    Se implememta la interface crudRepository

--- Spring MVC & Data JPA/spring-boot-data-jpa/src/main/java/com/rhm/models/dao/IClienteDao.java
+++ Spring MVC & Data JPA/spring-boot-data-jpa/src/main/java/com/rhm/models/dao/IClienteDao.java
@@ -1,18 +1,8 @@
 package com.rhm.models.dao;
 
-import java.util.List;
-
+import org.springframework.data.repository.CrudRepository;
 import com.rhm.models.entity.Cliente;
+//convertimos esta interface en un repositorio 
+public interface IClienteDao extends CrudRepository<Cliente, Long >{
 
-public interface IClienteDao {
-
-	// es una firma sin implementar solo indica en nombre del metodo
-	public List<Cliente> findAll();
-	//insertar cliente 
-	public void save(Cliente cliente);
-	//editar cliente
-	public Cliente findOne(Long id);
-	//eliminar cliente
-	public void delete(Long id);
-	
 }
